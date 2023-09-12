7110c8356ec5df9867f4e66efc416516a57db4a6
Slava Maksimov <hello@slavamak.com>
2023-09-12 20:52:38 +0300

    feat(import-maps): add base url

--- src/import-maps.ts
+++ src/import-maps.ts
@@ -1,4 +1,4 @@
-import type { Plugin } from 'vite'
+import type { Plugin, ResolvedConfig } from 'vite'
 import path from 'node:path'
 import fs from 'node:fs/promises'
 import type { PluginOptions } from './types'
@@ -13,35 +13,39 @@ export default function importMaps (options: PluginOptions | undefined): Plugin 
   const defaultFilename = 'importmap.liquid'
   const filename = options?.filename ?? defaultFilename
 
+  let config: ResolvedConfig
+
   return {
     name: 'vite-plugin-shopify-import-maps:import-maps',
-    writeBundle: {
-      order: 'post',
-      async handler (_, bundle) {
-        const outDir = path.resolve(options?.outDir ?? './snippets')
-        const importMapFile = path.join(outDir, filename)
-        const importMap = new Map()
+    apply: 'build',
+    enforce: 'post',
+    configResolved (resolvedConfig) {
+      config = resolvedConfig
+    },
+    async writeBundle (_, bundle) {
+      const outDir = path.resolve(options?.outDir ?? './snippets')
+      const importMapFile = path.join(outDir, filename)
+      const importMap = new Map<string, string>()
 
-        await Promise.allSettled(
-          Object.keys(bundle).map(async (fileName) => {
-            if (fileName.endsWith('manifest.json')) {
-              return
-            }
+      await Promise.allSettled(
+        Object.keys(bundle).map(async (fileName) => {
+          if (fileName.endsWith('manifest.json')) {
+            return
+          }
 
-            importMap.set(fileName, `{{ '${fileName}' | asset_url }}`)
-          })
-        )
+          importMap.set(`${config.base}${fileName}`, `{{ '${fileName}' | asset_url }}`)
+        })
+      )
 
-        const json = JSON.stringify({ imports: Object.fromEntries(importMap) }, null, 2)
+      const json = JSON.stringify({ imports: Object.fromEntries(importMap) }, null, 2)
 
-        await fs.writeFile(
-          importMapFile,
-          `<script type="importmap">\n${json}\n</script>`,
-          { encoding: 'utf8' }
-        )
+      await fs.writeFile(
+        importMapFile,
+        `<base href="{{ 'base' | asset_url | split: 'base' | first }}">\n<script type="importmap">\n${json}\n</script>`,
+        { encoding: 'utf8' }
+      )
 
-        console.info(`[import-maps]: Successfully wrote to ${importMapFile}`)
-      }
+      console.info(`[import-maps]: Successfully wrote to ${importMapFile}`)
     }
   }
 }
